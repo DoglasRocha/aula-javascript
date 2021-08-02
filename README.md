429dca2ef80a8d6104e354ad18370f9986ee3f4d
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-02 14:03:03 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aula de Introdução ao JavaScript
+
+Repositório criado para a aula de introdução a JavaScript da Digital Innovation One (DIO).
---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = 'Doglas Rocha';
+var idade = 18;
+var idade2 = 10;
+var frase = 'Ferrari é a melhor equipe';
+
+// alert(nome + " tem " + idade + ' anos');
+// alert(idade + idade2)
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
