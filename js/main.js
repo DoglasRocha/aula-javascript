e6a4a5501c70318ad0b25f7877e319bf8e42cefd
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-02 22:34:03 -0300

    terceira aula do curso

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ alert(nome + " tem " + idade + ' anos');
 alert(idade + idade2)
 console.log(nome);
 console.log(idade + idade2);
-console.log(frase.toUpperCase()); */
+console.log(frase.toUpperCase());
 
-/* var array = ['pedra', 'papel'];
+var array = ['pedra', 'papel'];
 array.push('tesoura');
 array.pop()
 console.log(array);
@@ -16,7 +16,38 @@ console.log(array);
 console.log(array.reverse());
 console.log(array.length);
 console.log(array.toString());
-console.log(array.join(' - ')) */
+console.log(array.join(' - ')) 
 
 var fruta = {nome: 'uva', cor: 'roxa'};
-console.log()
+console.log() 
+
+var idade = prompt('Qual sua idade? ');
+var temCarteira = prompt('Você tem carteira de motorista? ');
+
+if (temCarteira.toLowerCase() == 'sim') {
+    temCarteira = true;
+} else {
+    temCarteira = false;
+}
+
+if (idade >= 18 && temCarteira) {
+    alert('Você pode dirigir!')
+} else {
+    alert('Você não pode dirigir!')
+} 
+
+var count = 0;
+
+while (count <= 5) {
+    console.log(count);
+    count++;
+}
+
+for(var count = 0; count <= 5; count++) {
+    alert(count)
+} 
+
+var d = new Date();
+alert(d.getMonth() + 1);
+alert(d.getHours());
+alert(d.getFullYear()); */
