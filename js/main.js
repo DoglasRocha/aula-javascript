748d56ecd7cd618123de0c9e4b03b9075ae04256
DoglasRocha <doglas_rocha@protonmail.ch>
2021-08-02 22:10:24 -0300

    segunda aula do curso

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
-var nome = 'Doglas Rocha';
+/* var nome = 'Doglas Rocha';
 var idade = 18;
 var idade2 = 10;
 var frase = 'Ferrari é a melhor equipe';
-
-// alert(nome + " tem " + idade + ' anos');
-// alert(idade + idade2)
-
+alert(nome + " tem " + idade + ' anos');
+alert(idade + idade2)
 console.log(nome);
 console.log(idade + idade2);
-console.log(frase.toUpperCase());
+console.log(frase.toUpperCase()); */
+
+/* var array = ['pedra', 'papel'];
+array.push('tesoura');
+array.pop()
+console.log(array);
+
+console.log(array.reverse());
+console.log(array.length);
+console.log(array.toString());
+console.log(array.join(' - ')) */
+
+var fruta = {nome: 'uva', cor: 'roxa'};
+console.log()
